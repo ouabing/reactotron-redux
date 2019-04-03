2a9cf20b2aec845623bfe7ee48ac75fcd62ca739
abing <abing@gcores.com>
2019-04-03 16:59:20 +0800

    Omit realmData

--- src/sendAction.ts
+++ src/sendAction.ts
@@ -3,7 +3,13 @@ import { Reactotron } from "reactotron-core-client";
 export default function createSendAction(reactotron: Reactotron) {
   return (action: { type: any }, ms: number, important = false) => {
     // let's call the type, name because that's "generic" name in Reactotron
-    let { type: name } = action
+    let { type: name, payload } = action
+    let newAction = { ...action }
+    if (payload) {
+      let newPayload = {...payload}
+      delete newPayload.realmData
+      newAction.payload = newPayload
+    }
 
     // convert from symbol to type if necessary
     if (typeof name === "symbol") {
@@ -13,7 +19,9 @@ export default function createSendAction(reactotron: Reactotron) {
         .replace(/\)$/, "")
     }
 
+
     // off ya go!
-    reactotron.send("state.action.complete", { name, action, ms }, important)
+    // reactotron.send("state.action.complete", { name, action, ms }, important)
+    reactotron.send("state.action.complete", { name, action: newAction, ms }, important)
   }
 }
